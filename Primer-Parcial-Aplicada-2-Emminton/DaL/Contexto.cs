6a3ec174723d81d1d9b99068e12cbbf0d64cf92f
Emminton Manuel Ure�a Santana <emminton_urena@ucne.edu.do>
2020-06-10 20:49:32 -0400

    Agregando Contexto

---
+++ Primer-Parcial-Aplicada-2-Emminton/DaL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Primer_Parcial_Aplicada_2_Emminton.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Primer_Parcial_Aplicada_2_Emminton.DaL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulo> Articulos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Data\Articulos.db");
+        }
+    }
+}
